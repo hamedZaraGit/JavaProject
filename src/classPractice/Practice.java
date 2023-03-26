cd33e1d32ec71e779d0c1117369d14fbb6480638
hamed <hamed.zara@tekschool.us>
2023-03-25 18:51:37 -0700

    addded new classes in Encapsulation and Fib numbers practice

--- src/classPractice/Practice.java
+++ src/classPractice/Practice.java
@@ -1,12 +1,27 @@
 package classPractice;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Practice {
 	public static void main (String[] args) {
-
 		
+		
+		
+			  
+			  double sum = (50 + 50 + 50);
+			  
+			
+			
+			  
+			  
+			  double averagePercentage = (sum) % 3;
+			  
+			  System.out.println(averagePercentage);
+			
 	}
+
 
 }
--- src/date_12_12_22_Encapsulation/IQ_FibNumbers.java
+++ src/date_12_12_22_Encapsulation/IQ_FibNumbers.java
@@ -14,22 +14,53 @@ public class IQ_FibNumbers {
 		// the function will take one int as input
 		
 		public ArrayList<Integer> fibNumsGenerator(int n){
+			// i will create an ArrayList of numbers
 			ArrayList<Integer> numbers = new ArrayList<>();
 			
-			numbers.add(1);
-			numbers.add(1);
+			// one way of doing it
 			
+			if(n == 0) {
+				return numbers;
+			}
+			else if(n == 1) {
+				numbers.add(1);
+			}
+			else{
+				numbers.add(1);
+				numbers.add(1);
+			}
+			
+			
+			
+			
+//			This is another way of doing it: 
+			
+			// i will add 1 and 1 to it
+//			numbers.add(1);
+//			numbers.add(1);
+			// i will loop through for n-2 times
+			for (int i = 2; i < n; i++) {
+				int x = numbers.get(i-1) + numbers.get(i-2);
+				numbers.add(x);
+			}
+			// n=5 means n = [1, 1, 2, 3, 5]
 			return numbers;
 		}
-		
-		
-		
-		// i will create an arraylist of numbers
-		// i will 1 and 1 to it
-		// i will loop through for n-2 times
 		// i will start at index 2
 		// on each iteration, i will add two previous indexes
-		// i will return the arraylist of ints
+		// i will return the ArrayList of int's
+		
+		
+		
+		
+		
+		
+		
+		public static void main (String[] args) {
+			IQ_FibNumbers temp = new IQ_FibNumbers();
+			System.out.println(temp.fibNumsGenerator(10));
+		}
+		
 		
 		
 		
